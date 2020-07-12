24342cbd9666852306cc97b9e5f1cdd8110859fd
b3nd3r-ssbm <67122986+b3nd3r-ssbm@users.noreply.github.com>
2020-07-12 14:10:48 -0400

    Create p5.js canvas
    for stages and characters to be shown on

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 var frames;
+var stage;
+var starting=false;
 if(app!== undefined){
 	app.on('ready',function(){
 		let win = new BrowserWindow({ 
@@ -15,4 +17,50 @@ function dropHandler(ev) {
 	const { default: SlippiGame } = require('slp-parser-js');
 	const game = new SlippiGame(Buffer.from(ev.dataTransfer.items[0].getAsFile()));
 	frames=game.getFrames();
+	const settings=game.getSettings();
+	stage=settings.stageId;
+	starting=true;
+}
+function setup(){
+	createCanvas(640,528);
+	loop();
+	background(255);
+}
+function stages(){
+	if(stage===2){
+		fod();
+	}
+	else if(stage===3){
+		ps();
+	}
+	else if(stage===8){
+		ys();
+	}
+	else if(stage===28){
+		dl();
+	}
+	else if(stage===31){
+		bf();
+	}
+	else if(stage===32){
+		fd();
+	}
+}
+function fd(){
+	
+}
+function ps(){
+	
+}
+function fod(){
+	
+}
+function bf(){
+	
+}
+function dl(){
+	
+}
+function ys(){
+	
 }
