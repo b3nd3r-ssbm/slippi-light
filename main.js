73d7b91f8c694136520aef41eb8811640a352dbb
b3nd3r-ssbm <67122986+b3nd3r-ssbm@users.noreply.github.com>
2020-07-15 15:37:07 -0400

    Fixed Stadium Platform Heights

--- main.js
+++ main.js
@@ -130,8 +130,8 @@ function ps(){
 	fill(0);
 	noStroke();
 	rect(284.5, addY, 351,10);
-	rect(350,335,30,5);
-	rect(570,335,30,5);
+	rect(350,310,30,5);
+	rect(570,310,30,5);
 }
 function fod(){
 	
